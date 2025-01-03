e6fd88313f3e7fb23f18c78ef1e33c7c8e7fd800
Merry <svgr1808@outlook.de>
2025-01-03 21:20:13 +0100

    Configure to support paper
    Signed-off-by: Merry <svgr1808@outlook.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
 plugins {
     id("java")
+    id("net.minecrell.plugin-yml.bukkit") version "0.6.0"
 }
 
-group = "org.breezora"
-version = "1.0.0"
+group = "net.alphalightning"
+version = "1.0.0-alpha.1"
+description = "Simple BedWars plugin to demonstrate jira"
 
 repositories {
-    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(23))
+    }
+}
+
+bukkit {
+    main = "net.alphalightning.bedwars.BedWarsPlugin"
+    apiVersion = "1.21"
+    foliaSupported = false
+    author = "Merry"
 }
