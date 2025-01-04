335cb3d918d16498da4adf8677cb242117bde13e
Max <mausetot467@gmail.com>
2025-01-04 17:10:18 +0100

    Add registerCommand Method

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -1,6 +1,18 @@
 package net.alphalightning.bedwars;
 
+import co.aikar.commands.PaperCommandManager;
+import net.alphalightning.bedwars.commands.SetSpawnPointCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BedWarsPlugin extends JavaPlugin {
+
+    public void onEnable() {
+        registerCommands();
+    }
+
+    public void registerCommands() {
+        PaperCommandManager manager = new PaperCommandManager(new BedWarsPlugin());
+
+        manager.registerCommand(new SetSpawnPointCommand());
+    }
 }
