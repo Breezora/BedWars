1dff612d7e36d8edf4ea35fa1de05df855a788b6
Merry <svgr1808@outlook.de>
2025-01-09 21:54:09 +0100

    Add jackson-paper as dependency

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -1,6 +1,10 @@
 package net.alphalightning.bedwars;
 
 import co.aikar.commands.PaperCommandManager;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import de.eldoria.jacksonbukkit.JacksonPaper;
 import net.alphalightning.bedwars.commands.CreateMapCommand;
 import net.alphalightning.bedwars.commands.SetSpawnPointCommand;
 import net.alphalightning.bedwars.translation.PluginTranslationRegistry;
@@ -9,6 +13,7 @@ import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationRegistry;
 import net.kyori.adventure.util.UTF8ResourceBundleControl;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -16,6 +21,10 @@ import java.util.ResourceBundle;
 public class BedWarsPlugin extends JavaPlugin {
 
     private final TranslationRegistry translationRegistry = new PluginTranslationRegistry(TranslationRegistry.create(Key.key("bedwars:messages")));
+    private final ObjectMapper mapper = JsonMapper.builder()
+            .addModule(JacksonPaper.builder().build())
+            .enable(SerializationFeature.INDENT_OUTPUT) // Pretty printing
+            .build();
 
     @Override
     public void onLoad() {
@@ -46,5 +55,9 @@ public class BedWarsPlugin extends JavaPlugin {
 
         manager.registerCommand(new SetSpawnPointCommand());
         manager.registerCommand(new CreateMapCommand());
+    }
+
+    public @NotNull ObjectMapper jsonMapper() {
+        return mapper;
     }
 }
