f9ee0fb5df8de0cdb04b683dff252d3080a2b6b5
Merry <svgr1808@outlook.de>
2025-01-09 16:32:25 +0100

    Prepare for removal

--- src/main/java/net/alphalightning/bedwars/commands/SetSpawnPointCommand.java
+++ src/main/java/net/alphalightning/bedwars/commands/SetSpawnPointCommand.java
@@ -1,13 +1,13 @@
 package net.alphalightning.bedwars.commands;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.*;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Description;
+import co.aikar.commands.annotation.Subcommand;
 import com.google.gson.JsonObject;
-import net.alphalightning.bedwars.util.JSONUtil;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.io.IOException;
 
 @CommandAlias("setspawnpoint")
 @Description("Set the Spawnpoints based on further Arguments")
@@ -17,24 +17,24 @@ public class SetSpawnPointCommand extends BaseCommand {
     @CommandPermission("bedwars.admin")
     public void onSetSpawnPoint(CommandSender sender) throws Exception {
 
-        if(sender instanceof Player p) {
-            try {
-                String filePath = "plugins/BedWars/config/lobbyconfig.json";
+        if (sender instanceof Player p) {
+//            try {
+            String filePath = "plugins/BedWars/config/lobbyconfig.json";
 
-                // Neues JSON-Objekt erstellen
-                JsonObject jsonObject = new JsonObject();
-                jsonObject.addProperty("x", p.getX());
-                jsonObject.addProperty("y", p.getY());
-                jsonObject.addProperty("z", p.getZ());
-                jsonObject.addProperty("pitch", p.getPitch());
-                jsonObject.addProperty("yaw", p.getYaw());
-                jsonObject.addProperty("world", p.getWorld().getName());
-                // In Datei schreiben
-                JSONUtil.writeJsonToFile(filePath, jsonObject);
-                System.out.println("JSON-Datei wurde geschrieben.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            // Neues JSON-Objekt erstellen
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("x", p.getX());
+            jsonObject.addProperty("y", p.getY());
+            jsonObject.addProperty("z", p.getZ());
+            jsonObject.addProperty("pitch", p.getPitch());
+            jsonObject.addProperty("yaw", p.getYaw());
+            jsonObject.addProperty("world", p.getWorld().getName());
+            // In Datei schreiben
+            //    JSONUtil.writeJsonToFile(filePath, jsonObject);
+            System.out.println("JSON-Datei wurde geschrieben.");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
     }
 }
