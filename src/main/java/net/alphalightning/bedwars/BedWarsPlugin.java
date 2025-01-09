bddf3234e4d993a3c70059f54c4cf06c66a85b79
Merry <svgr1808@outlook.de>
2025-01-09 20:57:51 +0100

    Register main class as dependency

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -42,6 +42,8 @@ public class BedWarsPlugin extends JavaPlugin {
 
     public void registerCommands() {
         PaperCommandManager manager = new PaperCommandManager(this);
+        manager.registerDependency(BedWarsPlugin.class, this);
+
         manager.registerCommand(new SetSpawnPointCommand());
         manager.registerCommand(new CreateMapCommand());
     }
