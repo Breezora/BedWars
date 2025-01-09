0d0308ef7e9afe9268f019a494883dbd61a6cc6a
Merry <svgr1808@outlook.de>
2025-01-09 21:00:29 +0100

    Implement stage startup logic

--- src/main/java/net/alphalightning/bedwars/setup/map/LobbyMapSetup.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/LobbyMapSetup.java
@@ -1,18 +1,26 @@
 package net.alphalightning.bedwars.setup.map;
 
+import net.alphalightning.bedwars.BedWarsPlugin;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.logger.slf4j.ComponentLogger;
 import org.bukkit.entity.Player;
 
 public final class LobbyMapSetup implements MapSetup {
 
-    private final Player player;
+    private final ComponentLogger logger;
 
-    public LobbyMapSetup(Player player) {
+    private final Player player;
+    private int stage;
+
+    public LobbyMapSetup(BedWarsPlugin plugin, Player player) {
+        this.logger = plugin.getComponentLogger();
         this.player = player;
+        startStage(0);
     }
 
     @Override
     public void start() {
-
+        startStage(1);
     }
 
     @Override
@@ -23,5 +31,20 @@ public final class LobbyMapSetup implements MapSetup {
     @Override
     public void saveConfiguration() {
 
+    }
+
+    private void startStage(int stage) {
+        if (this.stage > stage) {
+            throw new IllegalStateException("Could not start stage " + stage + ": The new stage must be at least the current stage (" + this.stage + ")or higher.");
+        }
+        this.stage = stage;
+
+        switch (stage) {
+            case 0 -> player.sendMessage(Component.translatable("lobbysetup.start"));
+            case 1 -> player.sendMessage(Component.translatable("lobbysetup.stage1"));
+            case 2 -> player.sendMessage(Component.translatable("lobbysetup.stage2"));
+            case 3 -> player.sendMessage(Component.translatable("lobbysetup.finish"));
+            default -> logger.warn(Component.translatable("lobbysetup.cancel"));
+        }
     }
 }
