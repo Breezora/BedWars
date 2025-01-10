01c33f959c6903ae4bad7741533c16c62128dc03
Merry <svgr1808@outlook.de>
2025-01-10 16:44:16 +0100

    Start implementation of overview ui

---
+++ src/main/java/net/alphalightning/bedwars/setup/ui/GameMapConfigurationOverviewGui.java
@@ -0,0 +1,38 @@
+package net.alphalightning.bedwars.setup.ui;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import xyz.xenondevs.invui.gui.PagedGui;
+import xyz.xenondevs.invui.item.Item;
+import xyz.xenondevs.invui.item.ItemBuilder;
+import xyz.xenondevs.invui.window.Window;
+import xyz.xenondevs.invui.window.Window.Builder.Normal.Single;
+
+public class GameMapConfigurationOverviewGui {
+
+    private final Player owner;
+    private final Single gui;
+
+    public GameMapConfigurationOverviewGui(Player owner) {
+        this.owner = owner;
+        this.gui = createGui();
+    }
+
+    private Single createGui() {
+        return Window.single().setGui(PagedGui.guis()
+                .setStructure(
+                        ". . . . . . . . .",
+                        ". . . a . b . . .",
+                        ". . . . . . . . ."
+                )
+                .addIngredient('a', Item.simple(new ItemBuilder(Material.RED_BED)))
+                .addIngredient('b', Item.simple(new ItemBuilder(Material.TRIAL_SPAWNER)))
+                .build()
+        ).setTitle(Component.translatable("mapsetup.gui.overview.title"));
+    }
+
+    public void showGui() {
+        gui.open(owner);
+    }
+}
