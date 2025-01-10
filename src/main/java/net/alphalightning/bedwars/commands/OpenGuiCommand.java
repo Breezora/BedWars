cd737bf24174cae32407da886cae3e939dd1e8cd
Merry <svgr1808@outlook.de>
2025-01-10 16:29:48 +0100

    Remove all invui related implementations

--- src/main/java/net/alphalightning/bedwars/commands/OpenGuiCommand.java
+++ src/main/java/net/alphalightning/bedwars/commands/OpenGuiCommand.java
@@ -3,7 +3,6 @@ package net.alphalightning.bedwars.commands;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.Default;
-import net.alphalightning.bedwars.setup.ui.GameMapSetupOverviewGui;
 import org.bukkit.entity.Player;
 
 /**
@@ -14,7 +13,6 @@ public class OpenGuiCommand extends BaseCommand {
 
     @Default
     public void onDefault(Player player) {
-        new GameMapSetupOverviewGui(player).openGui();
     }
 
 }
