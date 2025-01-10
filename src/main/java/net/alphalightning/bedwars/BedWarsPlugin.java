1eda7762039e9010e24238f1725b0607aca6577c
Merry <svgr1808@outlook.de>
2025-01-10 12:07:12 +0100

    Remove obsolete command

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import de.eldoria.jacksonbukkit.JacksonPaper;
 import net.alphalightning.bedwars.commands.CreateMapCommand;
-import net.alphalightning.bedwars.commands.SetSpawnPointCommand;
 import net.alphalightning.bedwars.translation.PluginTranslationRegistry;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.translation.GlobalTranslator;
@@ -56,7 +55,6 @@ public class BedWarsPlugin extends JavaPlugin {
     public void registerCommands() {
         PaperCommandManager manager = new PaperCommandManager(this);
 
-        manager.registerCommand(new SetSpawnPointCommand());
         manager.registerCommand(new CreateMapCommand());
     }
 
