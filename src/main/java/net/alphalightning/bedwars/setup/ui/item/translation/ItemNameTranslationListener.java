3804f45103919f3f78e7776b2324651c44160c4e
Merry <svgr1808@outlook.de>
2025-01-12 20:49:34 +0100

    Try to change item displayname

--- src/main/java/net/alphalightning/bedwars/setup/ui/item/translation/ItemNameTranslationListener.java
+++ src/main/java/net/alphalightning/bedwars/setup/ui/item/translation/ItemNameTranslationListener.java
@@ -2,14 +2,17 @@ package net.alphalightning.bedwars.setup.ui.item.translation;
 
 import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.protocol.item.ItemStack;
+import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
+import com.github.retrooper.packetevents.protocol.nbt.NBTString;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerWindowItems;
 import net.alphalightning.bedwars.BedWarsPlugin;
 import org.bukkit.entity.Player;
 
 public final class ItemNameTranslationListener implements PacketListener {
 
     private final BedWarsPlugin plugin;
-    private int count = 0; // Debug
 
     public ItemNameTranslationListener(BedWarsPlugin plugin) {
         this.plugin = plugin;
@@ -23,6 +26,24 @@ public final class ItemNameTranslationListener implements PacketListener {
             return;
         }
 
-        plugin.getLogger().info("Packet gesendet: " + ++count);
+        WrapperPlayServerWindowItems packet = new WrapperPlayServerWindowItems(event);
+        ItemStack item = packet.readItemStackModern();
+
+        if (item == null) {
+            return;
+        }
+
+        NBTCompound nbt = item.getOrCreateTag();
+        NBTCompound display = nbt.getCompoundTagOrNull("display");
+
+        if (display == null) {
+            display = new NBTCompound();
+        }
+        display.setTag("name", new NBTString("{\"text\":\"Test\"}"));
+        item.setNBT(display);
+
+        packet.writeItemStack(item);
+
+        plugin.getLogger().info("Paket aktualisiert");
     }
 }
