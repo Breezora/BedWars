c73f55a4e75944dfcb2919df4ea9051979bc80e4
Merry <56554514+MerryDev@users.noreply.github.com>
2025-01-15 23:14:41 +0100

    Revert "Revert invalid merge"

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -1,20 +1,34 @@
 package net.alphalightning.bedwars;
 
 import co.aikar.commands.PaperCommandManager;
-import net.alphalightning.bedwars.commands.SetSpawnPointCommand;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import de.eldoria.jacksonbukkit.JacksonPaper;
+import net.alphalightning.bedwars.commands.CreateMapCommand;
+import net.alphalightning.bedwars.commands.OpenGuiCommand;
 import net.alphalightning.bedwars.translation.PluginTranslationRegistry;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.translation.GlobalTranslator;
 import net.kyori.adventure.translation.TranslationRegistry;
 import net.kyori.adventure.util.UTF8ResourceBundleControl;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class BedWarsPlugin extends JavaPlugin {
 
-    private final TranslationRegistry translationRegistry = new PluginTranslationRegistry(TranslationRegistry.create(Key.key("bedwars", "messages")));
+    private final TranslationRegistry translationRegistry = new PluginTranslationRegistry(TranslationRegistry.create(Key.key("bedwars:messages")));
+    private final ObjectMapper mapper = JsonMapper.builder()
+            .addModule(JacksonPaper.builder().build())
+            .enable(SerializationFeature.INDENT_OUTPUT) // Pretty printing
+            .build()
+            .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+            .setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
 
     @Override
     public void onLoad() {
@@ -34,13 +48,19 @@ public class BedWarsPlugin extends JavaPlugin {
 
     private void loadMessageRegistry() {
         translationRegistry.defaultLocale(Locale.GERMAN);
-        translationRegistry.registerAll(Locale.GERMAN, ResourceBundle.getBundle("messages", Locale.GERMAN, UTF8ResourceBundleControl.get()), true);
+        translationRegistry.registerAll(Locale.GERMAN, ResourceBundle.getBundle("messages", Locale.GERMANY, UTF8ResourceBundleControl.get()), true);
 
         GlobalTranslator.translator().addSource(translationRegistry);
     }
 
     public void registerCommands() {
         PaperCommandManager manager = new PaperCommandManager(this);
-        manager.registerCommand(new SetSpawnPointCommand());
+
+        manager.registerCommand(new CreateMapCommand());
+        manager.registerCommand(new OpenGuiCommand()); // Debug command
+    }
+
+    public @NotNull ObjectMapper jsonMapper() {
+        return mapper;
     }
 }
