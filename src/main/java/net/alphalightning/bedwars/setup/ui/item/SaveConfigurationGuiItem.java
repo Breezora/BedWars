c29e7497f86bf6ffab8a5221476ef43165bdc404
Merry <svgr1808@outlook.de>
2025-01-16 22:56:12 +0100

    Change item type & Print debug messages

--- src/main/java/net/alphalightning/bedwars/setup/ui/item/SaveConfigurationGuiItem.java
+++ src/main/java/net/alphalightning/bedwars/setup/ui/item/SaveConfigurationGuiItem.java
@@ -5,6 +5,7 @@ import net.alphalightning.bedwars.setup.ui.GameMapConfigurationOverviewGui;
 import net.alphalightning.bedwars.setup.ui.SelectTeamsGui;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.translation.GlobalTranslator;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -12,12 +13,12 @@ import org.bukkit.event.inventory.ClickType;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
-import xyz.xenondevs.invui.item.AbstractItem;
+import xyz.xenondevs.invui.item.AbstractBoundItem;
 import xyz.xenondevs.invui.item.Click;
 import xyz.xenondevs.invui.item.ItemBuilder;
 import xyz.xenondevs.invui.item.ItemProvider;
 
-public class SaveConfigurationGuiItem extends AbstractItem {
+public class SaveConfigurationGuiItem extends AbstractBoundItem {
 
     private final NamespacedKey key = new NamespacedKey("bedwars", "stage");
     private final PersistentDataContainer container;
@@ -39,8 +40,14 @@ public class SaveConfigurationGuiItem extends AbstractItem {
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
         int stage = container.getOrDefault(key, PersistentDataType.INTEGER, 0);
 
+        Bukkit.getLogger().info("Stage: " + stage);
+
         if (stage == 1) {
-            if (SelectTeamsGui.selectedTeams().size() < 2) {
+            int size = SelectTeamsGui.selectedTeams().size();
+
+            Bukkit.getLogger().info("Size: " + size);
+
+            if (size < 2) {
                 player.sendMessage(Component.translatable("mapsetup.error.invalid-team-configuration"));
                 return;
             }
