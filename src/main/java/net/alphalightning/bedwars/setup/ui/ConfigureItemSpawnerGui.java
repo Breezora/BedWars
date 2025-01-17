b60046160cc269cfdcef24eccf4fd047e6996ea1
Merry <svgr1808@outlook.de>
2025-01-17 01:32:58 +0100

    Fix non closable gui

--- src/main/java/net/alphalightning/bedwars/setup/ui/ConfigureItemSpawnerGui.java
+++ src/main/java/net/alphalightning/bedwars/setup/ui/ConfigureItemSpawnerGui.java
@@ -1,14 +1,20 @@
 package net.alphalightning.bedwars.setup.ui;
 
+import net.alphalightning.bedwars.feedback.Feedback;
 import net.alphalightning.bedwars.setup.ui.item.DiamondSpawnerGuiItem;
 import net.alphalightning.bedwars.setup.ui.item.EmeraldSpawnerGuiItem;
 import net.alphalightning.bedwars.setup.ui.item.ReturnToOverviewGuiItem;
 import net.alphalightning.bedwars.setup.ui.item.SaveConfigurationGuiItem;
 import net.kyori.adventure.text.Component;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import xyz.xenondevs.invui.gui.Gui;
 import xyz.xenondevs.invui.window.Window;
 import xyz.xenondevs.invui.window.Window.Builder.Normal.Single;
+
+import java.util.Collections;
 
 public class ConfigureItemSpawnerGui {
 
@@ -41,7 +47,7 @@ public class ConfigureItemSpawnerGui {
                         .build()
                 )
                 .setTitle(Component.translatable("mapsetup.gui.configure-spawner.title"))
-                .setCloseable(false);
+                .setCloseHandlers(Collections.singletonList(this::handleClose));
     }
 
     public void showGui() {
@@ -54,5 +60,14 @@ public class ConfigureItemSpawnerGui {
 
     public static int diamondSpawnerCount() {
         return diamondSpawnerCount;
+    }
+
+    private void handleClose() {
+        PersistentDataContainer container = owner.getPersistentDataContainer();
+        NamespacedKey key = new NamespacedKey("bedwars", "stage");
+
+        int stage = container.getOrDefault(key, PersistentDataType.INTEGER, 0);
+        container.set(key, PersistentDataType.INTEGER, stage + 1);
+        Feedback.success(owner);
     }
 }
