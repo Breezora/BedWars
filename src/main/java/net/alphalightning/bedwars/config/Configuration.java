21ac1ce58b3a01a06250377feb40cddd22d1708b
Merry <svgr1808@outlook.de>
2025-01-18 22:06:24 +0100

    Add configuration system with default environment settings
    Implemented a basic configuration system using JacksonConfig and a `Default` class to handle environment settings. This setup includes support for initializing a default development environment.

---
+++ src/main/java/net/alphalightning/bedwars/config/Configuration.java
@@ -0,0 +1,19 @@
+package net.alphalightning.bedwars.config;
+
+import de.eldoria.eldoutilities.config.ConfigKey;
+import de.eldoria.eldoutilities.config.JacksonConfig;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class Configuration extends JacksonConfig<Default> {
+
+    private static final ConfigKey<Default> MAIN = ConfigKey.defaultConfig(Default.class, Default::new);
+
+    public Configuration(@NotNull Plugin plugin) {
+        super(plugin, MAIN);
+    }
+
+    public void createDefault() {
+        main().environment(Environment.DEVELOPMENT);
+    }
+}
