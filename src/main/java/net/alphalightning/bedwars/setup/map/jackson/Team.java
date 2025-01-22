2d1321b6f75f41f4958448c47a54cb085b75fd65
Merry <svgr1808@outlook.de>
2025-01-22 19:17:46 +0100

    Add spawnpoint support to Team class
    Introduced a new Location field to represent the team's spawnpoint. Updated constructors, getter, and setter methods to handle spawnpoint functionality. This enhancement supports more detailed team configurations.

--- src/main/java/net/alphalightning/bedwars/setup/map/jackson/Team.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/jackson/Team.java
@@ -1,17 +1,35 @@
 package net.alphalightning.bedwars.setup.map.jackson;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import org.bukkit.Location;
 
 public class Team {
 
     private final String name;
+    private Location spawnpoint;
 
     @JsonCreator
     public Team(String name) {
+        this(name, null);
+    }
+
+    @JsonCreator
+    public Team(String name, Location spawnpoint) {
         this.name = name;
+        this.spawnpoint = spawnpoint;
     }
 
     public String name() {
         return name;
     }
+
+    public Location spawnpoint() {
+        return spawnpoint;
+    }
+
+    public Team spawnpoint(Location spawnpoint) {
+        this.spawnpoint = spawnpoint;
+        return this;
+    }
+
 }
