b94b1b370f08a9a5681edbc56ad3a730f950b8fb
Merry <svgr1808@outlook.de>
2025-01-28 20:30:56 +0100

    Enhance debug message

--- src/main/java/net/alphalightning/bedwars/translation/ArgumentTag.java
+++ src/main/java/net/alphalightning/bedwars/translation/ArgumentTag.java
@@ -27,7 +27,7 @@ final class ArgumentTag implements TagResolver {
         final Map<String, ComponentLike> namedArgumentMap = new HashMap<>(this.argumentComponents.size());
 
         for (final ComponentLike argument : this.argumentComponents) {
-            System.out.println(argument);
+            System.out.println("Argumente: " + argument);
             if (argument instanceof VirtualComponent) {
                 System.out.println("Arg ist virtual");
                 final VirtualComponentRenderer<?> renderer = ((VirtualComponent) argument).renderer();
