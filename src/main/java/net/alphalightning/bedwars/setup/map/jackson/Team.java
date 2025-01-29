815fda114634204fc907c2c8a403394f5fe59d15
Max <mausetot467@gmail.com>
2025-01-29 12:25:58 +0100

    Add bed locations to Team class
    Extended the Team class to include bed_downside and bed_upside fields for better representation of bed positions. Updated constructors, getters, and setters to accommodate these new properties. This change ensures more precise bed data handling in the application.

--- src/main/java/net/alphalightning/bedwars/setup/map/jackson/Team.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/jackson/Team.java
@@ -8,17 +8,21 @@ public class Team {
     private final String name;
     private Location spawnpoint;
     private Location chest;
+    private Location bed_downside;
+    private Location bed_upside;
 
     @JsonCreator
     public Team(String name) {
-        this(name, null, null);
+        this(name, null, null, null, null);
     }
 
     @JsonCreator
-    public Team(String name, Location spawnpoint, Location chest) {
+    public Team(String name, Location spawnpoint, Location chest, Location bed_downside, Location bed_upside) {
         this.name = name;
         this.spawnpoint = spawnpoint;
         this.chest = chest;
+        this.bed_downside = bed_downside;
+        this.bed_upside = bed_upside;
     }
 
     public String name() {
@@ -29,8 +33,20 @@ public class Team {
         return spawnpoint;
     }
 
-    public Location chest() {
-        return chest;
+    public Location chest() { return chest; }
+
+    public Location bed_downside() { return bed_downside; }
+
+    public Location bed_upside() { return bed_upside; }
+
+    public Team bed_downside(Location bed_downside) {
+        this.bed_downside = bed_downside;
+        return this;
+    }
+
+    public Team bed_upside(Location bed_upside) {
+        this.bed_upside = bed_upside;
+        return this;
     }
 
     public Team chest(Location chest) {
