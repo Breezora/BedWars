cfe8376709f385c85dfa04d33a033487e9fe9034
Merry <svgr1808@outlook.de>
2025-01-31 13:32:18 +0100

    Refactor codebase

--- src/main/java/net/alphalightning/bedwars/translation/ArgumentTag.java
+++ src/main/java/net/alphalightning/bedwars/translation/ArgumentTag.java
@@ -47,23 +47,19 @@ final class ArgumentTag implements TagResolver {
     @Override
     public @Nullable Tag resolve(final @NotNull String name, final @NotNull ArgumentQueue arguments, final @NotNull Context ctx) throws ParsingException {
         if (name.equals(NAME) || name.equals(ALIAS)) {
-            final int index = arguments.popOr("No argument number provided")
-                    .asInt().orElseThrow(() -> ctx.newException("Invalid argument number", arguments));
+            final int index = arguments.popOr("No argument number provided").asInt().orElseThrow(() -> ctx.newException("Invalid argument number", arguments));
 
             if (index < 0 || index >= this.argumentComponents.size()) {
                 throw ctx.newException("Invalid argument number", arguments);
             }
-
             return this.withStyledComponent(this.argumentComponents.get(index));
 
         } else {
             final ComponentLike namedArgument = this.namedArguments.get(name);
-            if (namedArgument != null) {
-                return this.withStyledComponent(namedArgument);
-
-            } else {
+            if (namedArgument == null) {
                 return null;
             }
+            return this.withStyledComponent(namedArgument);
         }
     }
 
@@ -72,7 +68,7 @@ final class ArgumentTag implements TagResolver {
         return name.equals(NAME) || name.equals(ALIAS) || this.namedArguments.containsKey(name);
     }
 
-    private Tag withStyledComponent(ComponentLike argument) {
+    private @NotNull Tag withStyledComponent(ComponentLike argument) {
         if (argument instanceof Component component) {
             final Component styledComponent = component.style(component.style());
             return Tag.inserting(styledComponent);
