0d687350b2ee6cd9e3591b11c7c8409eb58df2c7
Merry <svgr1808@outlook.de>
2025-02-05 17:51:23 +0100

    Add support for Display Entities

--- src/main/java/net/alphalightning/bedwars/setup/visual/Visualizer.java
+++ src/main/java/net/alphalightning/bedwars/setup/visual/Visualizer.java
@@ -1,6 +1,7 @@
 package net.alphalightning.bedwars.setup.visual;
 
 import org.bukkit.Location;
+import org.bukkit.entity.Display;
 import org.jetbrains.annotations.NotNull;
 
 public interface Visualizer<T> {
@@ -8,5 +9,7 @@ public interface Visualizer<T> {
     void visualize(@NotNull T t);
 
     void spawnVisual(@NotNull Location location);
+
+    void renderDisplay(@NotNull Display display);
 
 }
