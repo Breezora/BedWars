41e49db1edd3531d90bb02aedbba814c21710c58
Max <mausetot467@gmail.com>
2025-02-05 23:06:03 +0100

    Add fastbuy gui to Paged Gui

--- src/main/java/net/alphalightning/bedwars/game/ui/ItemShopGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/ItemShopGui.java
@@ -4,8 +4,12 @@ import net.alphalightning.bedwars.game.ui.item.*;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import xyz.xenondevs.invui.gui.Gui;
+import xyz.xenondevs.invui.gui.PagedGui;
 import xyz.xenondevs.invui.window.Window;
 import xyz.xenondevs.invui.window.Window.Builder.Normal.Single;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ItemShopGui {
 
@@ -15,9 +19,11 @@ public class ItemShopGui {
         this.gui = createGui();
     }
 
+    Gui fastBuyGui = new FastBuyGui().gui();
+
     private Single createGui() {
         return Window.single()
-                .setGui(Gui.normal()
+                .setGui(PagedGui.guis()
                         .setStructure(
                                 "a b c d e f g h a",
                                 "x x x x x x x x x",
@@ -33,6 +39,7 @@ public class ItemShopGui {
                         .addIngredient('f', new BowsItem())
                         .addIngredient('g', new PotionsItem())
                         .addIngredient('h', new ExtrasItem())
+                        .setContent(List.of(fastBuyGui))
                         .build()
                 )
                 .setTitle(Component.translatable("gui.shop.itemshop.fastbuy.title"));
