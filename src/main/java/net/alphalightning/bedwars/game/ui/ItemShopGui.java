d1a24b09a7401fe17aa21d370d623736d647ee38
Max <mausetot467@gmail.com>
2025-02-05 23:11:17 +0100

    Register x char

--- src/main/java/net/alphalightning/bedwars/game/ui/ItemShopGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/ItemShopGui.java
@@ -4,6 +4,7 @@ import net.alphalightning.bedwars.game.ui.item.*;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
 import xyz.xenondevs.invui.gui.Gui;
+import xyz.xenondevs.invui.gui.Markers;
 import xyz.xenondevs.invui.gui.PagedGui;
 import xyz.xenondevs.invui.window.Window;
 import xyz.xenondevs.invui.window.Window.Builder.Normal.Single;
@@ -27,10 +28,11 @@ public class ItemShopGui {
                         .setStructure(
                                 "a b c d e f g h a",
                                 "x x x x x x x x x",
-                                ". x x x x x x x .",
-                                ". x x x x x x x .",
+                                "x x x x x x x x x",
+                                "x x x x x x x x x",
                                 ". . . . . . . . ."
                         )
+                        .addIngredient('x', Markers.CONTENT_LIST_SLOT_HORIZONTAL)
                         .addIngredient('a', new FastBuyItem())
                         .addIngredient('b', new BlocksItem())
                         .addIngredient('c', new CombatItem())
