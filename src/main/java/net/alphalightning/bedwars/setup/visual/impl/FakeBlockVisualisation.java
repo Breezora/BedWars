411dfa69b4d3f7bcd0b297db9b0e8062fc00f3ce
Merry <svgr1808@outlook.de>
2025-02-05 21:12:01 +0100

    Correct entity position

--- src/main/java/net/alphalightning/bedwars/setup/visual/impl/FakeBlockVisualisation.java
+++ src/main/java/net/alphalightning/bedwars/setup/visual/impl/FakeBlockVisualisation.java
@@ -14,11 +14,12 @@ public class FakeBlockVisualisation implements Visualisation<Location> {
 
     @Override
     public void show(@NotNull Location location) {
+        final Location blockLocation = location.toBlockLocation();
         final World world = location.getWorld();
 
         world.spawnEntity(location, EntityType.BLOCK_DISPLAY, SpawnReason.CUSTOM, entity -> {
             final BlockDisplay blockDisplay = (BlockDisplay) entity;
-            final Location displayLocation = location.subtract(0.5D, 0, 0.5D).addRotation(location.getYaw() * -1, location.getPitch() * -1);
+            final Location displayLocation = blockLocation.add(0.5D, 0, 0.5D).addRotation(location.getYaw() * -1, location.getPitch() * -1);
 
             blockDisplay.setBlock(Bukkit.createBlockData(Material.CHEST));
             blockDisplay.teleport(displayLocation);
