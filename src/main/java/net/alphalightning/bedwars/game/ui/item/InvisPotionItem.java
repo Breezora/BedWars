0d70fbc06a0078963e671aa614867448e688a13d
Max <mausetot467@gmail.com>
2025-02-06 16:36:40 +0100

    Get name to render correctly i guess

--- src/main/java/net/alphalightning/bedwars/game/ui/item/InvisPotionItem.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/item/InvisPotionItem.java
@@ -27,10 +27,11 @@ public class InvisPotionItem extends AbstractItem {
         Component lore = Component.translatable("gui.shop.itemshop.buyable.potion.invisibility.lore");
 
         return new ItemBuilder(Material.POTION)
+                .setName(GlobalTranslator.render(display, viewer.locale()))
                 .set(DataComponentTypes.POTION_CONTENTS, PotionContents.potionContents().potion(PotionType.INVISIBILITY)
                 .addCustomEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 20 * 30, 0)))
+                .set(DataComponentTypes.CUSTOM_NAME, display)
                 .set(DataComponentTypes.HIDE_ADDITIONAL_TOOLTIP)
-                .setName(GlobalTranslator.render(display, viewer.locale()))
                 .setLore(List.of(GlobalTranslator.render(price, viewer.locale()),
                         Component.empty(),
                         GlobalTranslator.render(lore, viewer.locale())))
