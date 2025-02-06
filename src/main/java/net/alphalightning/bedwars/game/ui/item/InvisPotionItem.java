f3f7c6c35337d3577c993e26dffe070bbaebeeba
Max <mausetot467@gmail.com>
2025-02-06 15:31:46 +0100

    Add Invis Potion

---
+++ src/main/java/net/alphalightning/bedwars/game/ui/item/InvisPotionItem.java
@@ -0,0 +1,44 @@
+package net.alphalightning.bedwars.game.ui.item;
+
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.PotionContents;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.translation.GlobalTranslator;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.potion.PotionType;
+import org.jetbrains.annotations.NotNull;
+import xyz.xenondevs.invui.item.AbstractItem;
+import xyz.xenondevs.invui.item.Click;
+import xyz.xenondevs.invui.item.ItemBuilder;
+import xyz.xenondevs.invui.item.ItemProvider;
+
+import java.util.List;
+
+public class InvisPotionItem extends AbstractItem {
+
+
+    @Override
+    public @NotNull ItemProvider getItemProvider(@NotNull Player viewer) {
+
+
+        Component display = Component.translatable("gui.shop.itemshop.buyable.potion.invisibility.name");
+        Component price = Component.translatable("gui.shop.itemshop.buyable.potion.invisibility.price");
+        Component lore = Component.translatable("gui.shop.itemshop.buyable.potion.invisibility.lore");
+
+
+        return new ItemBuilder(Material.POTION)
+                .set(DataComponentTypes.POTION_CONTENTS, PotionContents.potionContents().potion(PotionType.INVISIBILITY))
+                .setName(GlobalTranslator.render(display, viewer.locale()))
+                .setLore(List.of(GlobalTranslator.render(price, viewer.locale()),
+                        Component.empty(),
+                        GlobalTranslator.render(lore, viewer.locale())));
+
+    }
+
+    @Override
+    public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
+
+    }
+}
