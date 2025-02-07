3e053993d3e3ac3fbbb7c58dc2045d8aa228137e
Merry <svgr1808@outlook.de>
2025-02-08 00:26:27 +0100

    [BED-90] Use cloned location objects
    Took 5 minutes

--- src/main/java/net/alphalightning/bedwars/feedback/visual/impl/HologramVisualization.java
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/impl/HologramVisualization.java
@@ -38,16 +38,15 @@ public class HologramVisualization implements Visualization<Location> {
     public void show(@NotNull Location location) {
         final Location bottom = location.add(0, 0.5D, 0);
 
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(9))).render(new TextVisualization(Component.translatable("hologram.name")));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(8))).render(new TextVisualization(Component.translatable("hologram.all-time")));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(7))).render(new TextVisualization(Component.translatable("hologram.ranking", Component.text(this.ranking))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(6))).render(new TextVisualization(Component.translatable("hologram.kills", Component.text(this.kills))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(5))).render(new TextVisualization(Component.translatable("hologram.deaths", Component.text(this.deaths))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(4))).render(new TextVisualization(Component.translatable("hologram.kd", Component.text(this.kd))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(3))).render(new TextVisualization(Component.translatable("hologram.played-games", Component.text(this.playedGames))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET.multiply(2))).render(new TextVisualization(Component.translatable("hologram.wins", Component.text(this.wonGames))));
-        new TextRenderer(this.plugin, bottom.add(OFFSET)).render(new TextVisualization(Component.translatable("hologram.destroyed-beds", Component.text(this.destroyedBeds))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(9))).render(new TextVisualization(Component.translatable("hologram.name")));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(8))).render(new TextVisualization(Component.translatable("hologram.all-time")));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(7))).render(new TextVisualization(Component.translatable("hologram.ranking", Component.text(this.ranking))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(6))).render(new TextVisualization(Component.translatable("hologram.kills", Component.text(this.kills))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(5))).render(new TextVisualization(Component.translatable("hologram.deaths", Component.text(this.deaths))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(4))).render(new TextVisualization(Component.translatable("hologram.kd", Component.text(this.kd))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(3))).render(new TextVisualization(Component.translatable("hologram.played-games", Component.text(this.playedGames))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET.multiply(2))).render(new TextVisualization(Component.translatable("hologram.wins", Component.text(this.wonGames))));
+        new TextRenderer(this.plugin, bottom.clone().add(OFFSET)).render(new TextVisualization(Component.translatable("hologram.destroyed-beds", Component.text(this.destroyedBeds))));
         new TextRenderer(this.plugin, bottom).render(new TextVisualization(Component.translatable("hologram.win-rate", Component.text(this.winRate))));
-
     }
 }
