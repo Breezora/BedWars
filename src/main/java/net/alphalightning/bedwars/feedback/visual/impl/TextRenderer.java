4dab22320e455f40414045e7eac5545f3a149312
Merry <svgr1808@outlook.de>
2025-02-07 21:58:52 +0100

    [BED-112] Introduce text visualization
    Took 13 seconds

---
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/impl/TextRenderer.java
@@ -0,0 +1,24 @@
+package net.alphalightning.bedwars.feedback.visual.impl;
+
+import net.alphalightning.bedwars.BedWarsPlugin;
+import net.alphalightning.bedwars.feedback.visual.VisualizationRenderer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
+
+public class TextRenderer implements VisualizationRenderer<TextVisualization> {
+
+    private final BedWarsPlugin plugin;
+    private final Location location;
+
+    public TextRenderer(BedWarsPlugin plugin, Location location) {
+        this.plugin = plugin;
+        this.location = location;
+    }
+
+    @Override
+    public @NotNull BukkitTask render(@NotNull TextVisualization visualisation) {
+        return Bukkit.getScheduler().runTask(this.plugin, () -> visualisation.show(this.location));
+    }
+}
---
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/impl/TextVisualization.java
@@ -0,0 +1,31 @@
+package net.alphalightning.bedwars.feedback.visual.impl;
+
+import net.alphalightning.bedwars.feedback.visual.Visualization;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.TextDisplay;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.jetbrains.annotations.NotNull;
+
+public class TextVisualization implements Visualization<Location> {
+
+    private final Component text;
+
+    public TextVisualization(Component text) {
+        this.text = text;
+    }
+
+    @Override
+    public void show(@NotNull Location location) {
+        final World world = location.getWorld();
+
+        world.spawnEntity(location, EntityType.TEXT_DISPLAY, SpawnReason.CUSTOM, entity -> {
+            TextDisplay textDisplay = (TextDisplay) entity;
+            textDisplay.text(this.text);
+            textDisplay.setShadowed(false);
+            textDisplay.setSeeThrough(true);
+        });
+    }
+}
