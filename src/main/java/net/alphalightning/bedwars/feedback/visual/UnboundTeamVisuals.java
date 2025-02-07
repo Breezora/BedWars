d0dc9863cd35b0fb27f815f8b258c0276ca0534b
Merry <56554514+MerryDev@users.noreply.github.com>
2025-02-07 23:38:48 +0100

    Render fake item shop (#92)
    * [BED-112] Introduce entity visualization
    
    Took 24 minutes
    
    * [BED-112] Introduce text visualization
    
    Took 13 seconds
    
    * [BED-112] Render visual feedback
    
    Took 3 minutes
    
    * [BED-112] Enable gravity
    
    Took 5 minutes
    
    * [BED-112] Print location with offset into log
    
    Took 20 seconds
    
    * [BED-112] Remove head rotation
    
    Took 7 minutes
    
    * [BED-112] Apply physics
    
    Took 28 seconds
    
    * [BED-112] Use cloned location object
    
    Took 1 minute
    
    * [BED-112] Center locations
    
    Took 3 minutes
    
    * [BED-112] Render box last
    
    Took 2 minutes
    
    * [BED-112] Adjust entity location
    
    Took 3 minutes
    
    * [BED-112] Adjust villager location
    
    Took 3 minutes
    
    * [BED-112] Adjust location again
    
    Took 6 minutes
    
    * [BED-112] Adjust text location
    
    Took 4 minutes
    
    * [BED-112] Move text up
    
    Took 2 minutes
    
    * [BED-112] Move text up more
    
    Took 3 minutes
    
    * [BED-112] Lower text a little bit
    
    Took 3 minutes
    
    * [BED-112] Adjust text location again
    
    Took 5 minutes
    
    * [BED-112] Encapsulate name from villager
    
    Took 3 minutes
    
    * [BED-112] Reapply attributes
    
    Took 2 minutes
    
    * [BED-112] Adjust text location
    
    Took 14 seconds
    
    * [BED-112] Adjust villager location again
    
    Took 5 minutes
    
    * [BED-112] Move texts up
    
    Took 3 minutes
    
    * [BED-112] Render particles earlier
    
    Took 3 minutes
    
    * [BED-112] Adjust text location
    
    Took 3 minutes
    
    * [BED-112] Make entities invulnerable
    
    Took 3 minutes
    
    * [BED-112] Fix typo
    
    Took 1 minute
    
    * [BED-112] Refactor shop rendering
    
    Took 6 minutes

--- src/main/java/net/alphalightning/bedwars/feedback/visual/UnboundTeamVisuals.java
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/UnboundTeamVisuals.java
@@ -1,11 +1,11 @@
 package net.alphalightning.bedwars.feedback.visual;
 
 import net.alphalightning.bedwars.BedWarsPlugin;
-import net.alphalightning.bedwars.feedback.visual.impl.MultiBlockRenderer;
-import net.alphalightning.bedwars.feedback.visual.impl.MultiBlockVisualization;
-import net.alphalightning.bedwars.feedback.visual.impl.SingleLineRenderer;
-import net.alphalightning.bedwars.feedback.visual.impl.SingleLineVisualization;
+import net.alphalightning.bedwars.feedback.visual.impl.*;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +17,14 @@ public interface UnboundTeamVisuals {
         new MultiBlockRenderer(plugin, List.of(withOffset.getBlock(), withOffset.add(0, 1, 0).getBlock()))
                 .render(new MultiBlockVisualization(0xecb8f5));
         new SingleLineRenderer(plugin, player).render(new SingleLineVisualization(player));
+    }
+
+    static void renderShop(BedWarsPlugin plugin, Player player, @NotNull Location location, @NotNull Location withOffset, Component name) {
+        new EntityRenderer(plugin, location.toCenterLocation().subtract(0, 0.5D, 0)).render(new EntityVisualization(EntityType.VILLAGER, null));
+        new TextRenderer(plugin, withOffset.toCenterLocation().add(0, 1.7D, 0)).render(new TextVisualization(Component.translatable("entity.interact")));
+        new TextRenderer(plugin, withOffset.toCenterLocation().add(0, 1.95D, 0)).render(new TextVisualization(name));
+        Bukkit.getScheduler().runTaskLater(plugin, () -> UnboundTeamVisuals.renderSpawnpoint(plugin, player, withOffset), 1L); // Render this later to avoid some stupid collisions with the texts and villager
+
     }
 
 }
---
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/impl/TextRenderer.java
@@ -0,0 +1,29 @@
+package net.alphalightning.bedwars.feedback.visual.impl;
+
+import net.alphalightning.bedwars.BedWarsPlugin;
+import net.alphalightning.bedwars.feedback.visual.VisualizationRenderer;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.scheduler.BukkitTask;
+import org.jetbrains.annotations.NotNull;
+
+public class TextRenderer implements VisualizationRenderer<TextVisualization> {
+
+    private final BedWarsPlugin plugin;
+    private final Location location;
+
+    public TextRenderer(BedWarsPlugin plugin, Location location) {
+        this.plugin = plugin;
+        this.location = location;
+    }
+
+    @Override
+    public @NotNull BukkitTask render(@NotNull TextVisualization visualisation) {
+        return Bukkit.getScheduler().runTask(this.plugin, () -> {
+            final Location withoutHeadRotation = this.location.clone();
+            withoutHeadRotation.setPitch(0f);
+
+            visualisation.show(withoutHeadRotation);
+        });
+    }
+}
