84115721192c3e2750cb071c239f123c3671c8cc
Merry <svgr1808@outlook.de>
2025-02-07 22:45:12 +0100

    [BED-112] Move text up more
    Took 3 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/stages/gamemap/ShopVillagerConfigurationStage.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/stages/gamemap/ShopVillagerConfigurationStage.java
@@ -77,7 +77,7 @@ public class ShopVillagerConfigurationStage extends Stage implements LocationCon
 
         UnboundTeamVisuals.renderSpawnpoint(plugin, player, withOffset);
         new EntityRenderer(plugin, location.toCenterLocation().subtract(0, 1.5D, 0)).render(new EntityVisualization(EntityType.VILLAGER, Component.translatable("entity.interact")));
-        new TextRenderer(plugin, withOffset.toCenterLocation().add(0, 0.75D, 0)).render(new TextVisualization(Component.translatable("entity.villager.shop.item")));
+        new TextRenderer(plugin, withOffset.toCenterLocation().add(0, 1D, 0)).render(new TextVisualization(Component.translatable("entity.villager.shop.item")));
 
         player.sendMessage(Component.translatable("mapsetup.stage.12.name.success", Component.text(phase)));
         Feedback.success(player);
