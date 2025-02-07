fe11e0634cf62c2396636afee8c1570d75224ace
Merry <56554514+MerryDev@users.noreply.github.com>
2025-02-07 20:20:47 +0100

    Revert "Rewrite server_id selection in personal dev deployment workflow (#87)" (#88)
    This reverts commit e9f2a7d34675f0d0f158c4aaaada656d979effc0.

--- src/main/java/net/alphalightning/bedwars/feedback/visual/VisualizationConfiguration.java
+++
@@ -1,8 +0,0 @@
-package net.alphalightning.bedwars.feedback.visual;
-
-public interface VisualizationConfiguration {
-
-    double STEP = 0.15D;
-    float SIZE = 0.75F;
-
-}
--- src/main/java/net/alphalightning/bedwars/setup/map/LobbyConfiguration.java
+++
@@ -1,8 +0,0 @@
-package net.alphalightning.bedwars.setup.map;
-
-public interface LobbyConfiguration {
-
-    String LOBBY_MAP_NAME = "lobby";
-    String LOBBY_FILE_NAME = LOBBY_MAP_NAME + ".json";
-
-}
