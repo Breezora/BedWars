fbc7bc0bdbd1f14d6d878b50cf2d43a31ac64c85
Max <mausetot467@gmail.com>
2025-02-08 11:49:46 +0100

    [BED-23] Add BlocksGui

---
+++ src/main/java/net/alphalightning/bedwars/game/ui/BlocksGui.java
@@ -0,0 +1,26 @@
+package net.alphalightning.bedwars.game.ui;
+
+import net.alphalightning.bedwars.game.ui.item.*;
+import xyz.xenondevs.invui.gui.Gui;
+
+public class BlocksGui {
+
+    public final Gui gui() {
+        return Gui.normal()
+                .setStructure(
+                        ". a . . . . . . .",
+                        ". b c d e f g h .",
+                        ". . . . . . . . ."
+                )
+                .addIngredient('a', new CurrentItem())
+                .addIngredient('b', new WoolItem())
+                .addIngredient('c', new TerracottaItem())
+                .addIngredient('d', new GlassItem())
+                .addIngredient('e', new EndstoneItem())
+                .addIngredient('f', new LadderItem())
+                .addIngredient('g', new PlanksItem())
+                .addIngredient('h', new ObsidianItem())
+                .build();
+    }
+
+}
