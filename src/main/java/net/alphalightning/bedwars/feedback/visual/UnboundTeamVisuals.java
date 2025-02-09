8b2dc2ac13dec566a1a8f9b609490c5450e980f1
Merry <svgr1808@outlook.de>
2025-02-09 15:10:23 +0100

    [BED-121] Refactor text renderings
    Took 14 minutes

--- src/main/java/net/alphalightning/bedwars/feedback/visual/UnboundTeamVisuals.java
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/UnboundTeamVisuals.java
@@ -27,8 +27,8 @@ public interface UnboundTeamVisuals {
 
     static void renderShop(BedWarsPlugin plugin, MapSetup setup, Player player, @NotNull Location location, @NotNull Location withOffset, Component name) {
         MANAGER.registerTask(setup, new EntityRenderer(plugin, setup, location.toCenterLocation().subtract(0, 0.5D, 0)).render(new EntityVisualization(setup, player, EntityType.VILLAGER, null, null)));
-        MANAGER.registerTask(setup, new TextRenderer(plugin, setup, withOffset.toCenterLocation().add(0, 1.7D, 0)).render(new TextVisualization(setup, Component.translatable("entity.interact"))));
-        MANAGER.registerTask(setup, new TextRenderer(plugin, setup, withOffset.toCenterLocation().add(0, 1.95D, 0)).render(new TextVisualization(setup, name)));
+        MANAGER.registerTask(setup, new EntityRenderer(plugin, setup, withOffset.toCenterLocation().add(0, 1.7D, 0)).render(new EntityVisualization(setup, player, EntityType.TEXT_DISPLAY, null, Component.translatable("entity.interact"))));
+        MANAGER.registerTask(setup, new EntityRenderer(plugin, setup, withOffset.toCenterLocation().add(0, 1.95D, 0)).render(new EntityVisualization(setup, player, EntityType.TEXT_DISPLAY, null, name)));
         MANAGER.registerTask(setup, Bukkit.getScheduler().runTaskLater(plugin, () -> UnboundTeamVisuals.renderSpawnpoint(plugin, setup, player, withOffset), 1L)); // Render this later to avoid some stupid collisions with the texts and villager
     }
 
