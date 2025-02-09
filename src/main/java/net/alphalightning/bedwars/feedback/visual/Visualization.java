d8f7cf2d2f3da20488cc8244a5d939243314440d
Merry <svgr1808@outlook.de>
2025-02-09 16:10:10 +0100

    [BED-121] Refactor constants
    Took 1 minute

--- src/main/java/net/alphalightning/bedwars/feedback/visual/Visualization.java
+++ src/main/java/net/alphalightning/bedwars/feedback/visual/Visualization.java
@@ -2,7 +2,10 @@ package net.alphalightning.bedwars.feedback.visual;
 
 import org.jetbrains.annotations.NotNull;
 
-public interface Visualization<T>  extends VisualizationConfiguration {
+public interface Visualization<T> {
+
+    double STEP = 0.15D;
+    float SIZE = 0.75F;
 
     void show(@NotNull T t);
 
