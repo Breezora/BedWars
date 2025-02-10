cf9f43bf188baf9ab2a0724b99152c47c4c3e2d5
Max <mausetot467@gmail.com>
2025-02-10 18:00:21 +0100

    [BED-23] Change ToolsGui to the new System.
    Took 26 seconds

--- src/main/java/net/alphalightning/bedwars/game/ui/ToolsGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/ToolsGui.java
@@ -1,14 +1,27 @@
 package net.alphalightning.bedwars.game.ui;
 
+import net.alphalightning.bedwars.game.ui.item.BuyableItem;
 import net.alphalightning.bedwars.game.ui.item.CurrentItem;
 import net.alphalightning.bedwars.game.ui.legacy.ShearsItem;
 import net.alphalightning.bedwars.game.ui.legacy.WoodenAxeItem;
 import net.alphalightning.bedwars.game.ui.legacy.WoodenPickaxeItem;
+import org.bukkit.Material;
 import xyz.xenondevs.invui.gui.Gui;
 
 public class ToolsGui {
 
     public final Gui gui() {
+
+        String level = "gui.shop.itemshop.upgradable.level.1";
+
+        String upgrade = "gui.shop.itemshop.buyable.lore.upgradable";
+        String upgrade2 = "gui.shop.itemshop.buyable.lore.upgradable.2";
+
+        String permrespawn = "gui.shop.itemshop.buyable.lore.permrespawn";
+        String permrespawn2 = "gui.shop.itemshop.buyable.lore.permrespawn.2";
+        String permrespawn3 = "gui.shop.itemshop.buyable.lore.permrespawn.3";
+
+
         return Gui.normal()
                 .setStructure(
                         ". . . . a . . . .",
@@ -16,9 +29,37 @@ public class ToolsGui {
                         ". . . . . . . . ."
                 )
                 .addIngredient('a', new CurrentItem())
-                .addIngredient('b', new ShearsItem())
-                .addIngredient('c', new WoodenPickaxeItem())
-                .addIngredient('d', new WoodenAxeItem())
+                .addIngredient('b', new BuyableItem(Material.SHEARS, "gui.shop.itemshop.buyable.shears.name", 1,
+                        "gui.shop.itemshop.buyable.shears.price",
+                        "",
+                        "gui.shop.itemshop.buyable.shears.lore",
+                        "gui.shop.itemshop.buyable.shears.lore.2",
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-iron"))
+                .addIngredient('c', new BuyableItem(Material.WOODEN_PICKAXE, "gui.shop.itemshop.buyable.woodpickaxe.name", 1,
+                        "gui.shop.itemshop.buyable.woodpickaxe.price",
+                        level,
+                        "",
+                        upgrade,
+                        upgrade2,
+                        "",
+                        permrespawn,
+                        permrespawn2,
+                        permrespawn3,
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-iron"))
+                .addIngredient('d', new BuyableItem(Material.WOODEN_AXE, "gui.shop.itemshop.buyable.woodaxe.name", 1,
+                        "gui.shop.itemshop.buyable.woodaxe.price",
+                        level,
+                        "",
+                        upgrade,
+                        upgrade2,
+                        "",
+                        permrespawn,
+                        permrespawn2,
+                        permrespawn3,
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-iron"))
                 .build();
     }
 
