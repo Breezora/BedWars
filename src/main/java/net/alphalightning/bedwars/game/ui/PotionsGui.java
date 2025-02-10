be20900b99d97a2e4c9d58883f442406acaf41e6
Max <mausetot467@gmail.com>
2025-02-10 17:46:09 +0100

    [BED-23] Adapt new system in PotionsGui
    Took 4 seconds

--- src/main/java/net/alphalightning/bedwars/game/ui/PotionsGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/PotionsGui.java
@@ -1,14 +1,20 @@
 package net.alphalightning.bedwars.game.ui;
 
+import net.alphalightning.bedwars.game.ui.item.BuyablePotionItem;
 import net.alphalightning.bedwars.game.ui.item.CurrentItem;
 import net.alphalightning.bedwars.game.ui.legacy.InvisPotionItem;
 import net.alphalightning.bedwars.game.ui.legacy.JumpBoostPotionItem;
 import net.alphalightning.bedwars.game.ui.legacy.SpeedPotionItem;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
 import xyz.xenondevs.invui.gui.Gui;
 
 public class PotionsGui {
 
     public final Gui gui() {
+
+        String emerald = "gui.shop.itemshop.buyable.lore.not-enough-emerald";
+
         return Gui.normal()
                 .setStructure(
                         ". . . . . . a . .",
@@ -16,9 +22,24 @@ public class PotionsGui {
                         ". . . . . . . . ."
                 )
                 .addIngredient('a', new CurrentItem())
-                .addIngredient('b', new SpeedPotionItem())
-                .addIngredient('c', new JumpBoostPotionItem())
-                .addIngredient('d', new InvisPotionItem())
+                .addIngredient('b', new BuyablePotionItem("gui.shop.itemshop.buyable.potion.speed.name", PotionType.SWIFTNESS, PotionEffectType.SPEED, 45, 1,
+                        "gui.shop.itemshop.buyable.potion.speed.price",
+                        "",
+                        "gui.shop.itemshop.buyable.potion.speed.lore",
+                        "",
+                        emerald))
+                .addIngredient('c', new BuyablePotionItem("gui.shop.itemshop.buyable.potion.jumpboost.name", PotionType.WATER_BREATHING, PotionEffectType.JUMP_BOOST, 45, 5,
+                        "gui.shop.itemshop.buyable.potion.jumpboost.price",
+                        "",
+                        "gui.shop.itemshop.buyable.potion.jumpboost.lore",
+                        "",
+                        emerald))
+                .addIngredient('d', new BuyablePotionItem("gui.shop.itemshop.buyable.potion.invisibility.name", PotionType.INVISIBILITY, PotionEffectType.INVISIBILITY, 30, 0,
+                        "gui.shop.itemshop.buyable.potion.invisibility.price",
+                        "",
+                        "gui.shop.itemshop.buyable.potion.invisibility.lore",
+                        "",
+                        emerald))
                 .build();
     }
 
