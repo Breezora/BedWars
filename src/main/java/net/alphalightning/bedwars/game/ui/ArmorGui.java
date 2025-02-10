e75404c6785f11fc34d6c64086b9cc0ffb0149c7
Max <mausetot467@gmail.com>
2025-02-10 16:43:42 +0100

    [BED-23] Add BuyableItem with example ArmorGui
    Took 15 minutes

--- src/main/java/net/alphalightning/bedwars/game/ui/ArmorGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/ArmorGui.java
@@ -1,12 +1,13 @@
 package net.alphalightning.bedwars.game.ui;
 
-import net.alphalightning.bedwars.game.ui.legacy.ChainArmorItem;
+import net.alphalightning.bedwars.game.ui.item.BuyableItem;
 import net.alphalightning.bedwars.game.ui.legacy.CurrentItem;
-import net.alphalightning.bedwars.game.ui.legacy.DiamondArmorItem;
-import net.alphalightning.bedwars.game.ui.legacy.IronArmorItem;
+import org.bukkit.Material;
 import xyz.xenondevs.invui.gui.Gui;
 
 public class ArmorGui {
+
+
 
     public final Gui gui() {
         return Gui.normal()
@@ -16,9 +17,27 @@ public class ArmorGui {
                         ". . . . . . . . ."
                 )
                 .addIngredient('a', new CurrentItem())
-                .addIngredient('b', new ChainArmorItem())
-                .addIngredient('c', new IronArmorItem())
-                .addIngredient('d', new DiamondArmorItem())
+                .addIngredient('b', new BuyableItem(Material.CHAINMAIL_BOOTS, "gui.shop.itemshop.buyable.armor.chain.name", 1,
+                        "gui.shop.itemshop.buyable.armor.chain.price",
+                        "",
+                        "gui.shop.itemshop.buyable.armor.chain.lore",
+                        "gui.shop.itemshop.buyable.armor.chain.lore.2",
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-iron"))
+                .addIngredient('c', new BuyableItem(Material.IRON_BOOTS, "gui.shop.itemshop.buyable.armor.iron.name", 1,
+                        "gui.shop.itemshop.buyable.armor.iron.price",
+                        "",
+                        "gui.shop.itemshop.buyable.armor.iron.lore",
+                        "gui.shop.itemshop.buyable.armor.iron.lore.2",
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-gold"))
+                .addIngredient('d', new BuyableItem(Material.DIAMOND_BOOTS, "gui.shop.itemshop.buyable.armor.diamond.name", 1,
+                        "gui.shop.itemshop.buyable.armor.diamond.price",
+                        "",
+                        "gui.shop.itemshop.buyable.armor.diamond.lore",
+                        "gui.shop.itemshop.buyable.armor.diamond.lore.2",
+                        "",
+                        "gui.shop.itemshop.buyable.lore.not-enough-emerald"))
                 .build();
     }
 
