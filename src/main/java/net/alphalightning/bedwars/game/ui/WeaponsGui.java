199ac42113459f862793faf383d28967fc37a6dc
Max <mausetot467@gmail.com>
2025-02-10 18:09:25 +0100

    [BED-23] Change WeaponsGui to use new System
    Took 5 minutes

--- src/main/java/net/alphalightning/bedwars/game/ui/WeaponsGui.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/WeaponsGui.java
@@ -1,12 +1,19 @@
 package net.alphalightning.bedwars.game.ui;
 
+import net.alphalightning.bedwars.game.ui.item.BuyableItem;
 import net.alphalightning.bedwars.game.ui.item.CurrentItem;
 import net.alphalightning.bedwars.game.ui.legacy.*;
+import org.bukkit.Material;
 import xyz.xenondevs.invui.gui.Gui;
 
 public class WeaponsGui {
 
     public final Gui gui() {
+
+        String iron = "gui.shop.itemshop.buyable.lore.not-enough-iron";
+        String gold = "gui.shop.itemshop.buyable.lore.not-enough-gold";
+        String emerald = "gui.shop.itemshop.buyable.lore.not-enough-emerald";
+
         return Gui.normal()
                 .setStructure(
                         ". . a . . . . . .",
@@ -14,10 +21,22 @@ public class WeaponsGui {
                         ". . . . . . . . ."
                 )
                 .addIngredient('a', new CurrentItem())
-                .addIngredient('b', new StoneSwordItem())
-                .addIngredient('c', new IronSwordItem())
-                .addIngredient('d', new DiamondSwordItem())
-                .addIngredient('e', new StickItem())
+                .addIngredient('b', new BuyableItem(Material.STONE_SWORD, "gui.shop.itemshop.buyable.stonesword.name", 1,
+                        "gui.shop.itemshop.buyable.stonesword.price",
+                        "",
+                        iron))
+                .addIngredient('c', new BuyableItem(Material.IRON_SWORD, "gui.shop.itemshop.buyable.ironsword.name", 1,
+                        "gui.shop.itemshop.buyable.ironsword.price",
+                        "",
+                        gold))
+                .addIngredient('d', new BuyableItem(Material.DIAMOND_SWORD, "gui.shop.itemshop.buyable.diamondsword.name", 1,
+                        "gui.shop.itemshop.buyable.diamondsword.price",
+                        "",
+                        emerald))
+                .addIngredient('e', new BuyableItem(Material.STICK, "gui.shop.itemshop.buyable.stick.name", 1,
+                        "gui.shop.itemshop.buyable.stick.price",
+                        "",
+                        gold))
                 .build();
     }
 
