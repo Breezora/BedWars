e61d6a021de22c181e17aba296f2c2478b6b279b
Merry <svgr1808@outlook.de>
2025-02-24 22:29:08 +0100

    [BED-126] Refactor title behavior
    Took 7 minutes

--- src/main/java/net/alphalightning/bedwars/game/countdown/LobbyCountdown.java
+++ src/main/java/net/alphalightning/bedwars/game/countdown/LobbyCountdown.java
@@ -3,6 +3,7 @@ package net.alphalightning.bedwars.game.countdown;
 import net.alphalightning.bedwars.BedWarsPlugin;
 import net.alphalightning.bedwars.feedback.Feedback;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -18,11 +19,12 @@ public class LobbyCountdown extends Countdown {
     protected void onTick(int timeLeft) {
         Bukkit.getServer().getOnlinePlayers().forEach(player -> {
             switch (timeLeft) {
-                case 5, 4, 3, 2, 1 -> player.showTitle(Title.title(Component.text(timeLeft), Component.empty()));
+                case 5, 4, 3, 2, 1 -> {
+                    player.showTitle(Title.title(Component.text(timeLeft).color(NamedTextColor.YELLOW), Component.empty()));
+                    Feedback.lower(player);
+                }
             }
-
             update(player, timeLeft);
-            Feedback.lower(player);
         });
     }
 
@@ -30,6 +32,7 @@ public class LobbyCountdown extends Countdown {
     protected void onFinish() {
         Bukkit.getServer().getOnlinePlayers().forEach(player -> {
             update(player, 0);
+            player.clearTitle();
             Feedback.pling(player);
         });
         Bukkit.broadcast(Component.translatable("countdown.lobby.finish"));
