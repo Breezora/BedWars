175076bd91c08472e81b4f912b419cd6afb28493
Merry <svgr1808@outlook.de>
2025-02-24 22:22:08 +0100

    [BED-126] Play visual effects
    Took 10 minutes

--- src/main/java/net/alphalightning/bedwars/game/countdown/LobbyCountdown.java
+++ src/main/java/net/alphalightning/bedwars/game/countdown/LobbyCountdown.java
@@ -1,7 +1,9 @@
 package net.alphalightning.bedwars.game.countdown;
 
 import net.alphalightning.bedwars.BedWarsPlugin;
+import net.alphalightning.bedwars.feedback.Feedback;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -14,18 +16,27 @@ public class LobbyCountdown extends Countdown {
 
     @Override
     protected void onTick(int timeLeft) {
-        Bukkit.getServer().getOnlinePlayers().forEach(player -> update(player, timeLeft));
+        Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+            switch (timeLeft) {
+                case 5, 4, 3, 2, 1 -> player.showTitle(Title.title(Component.text(timeLeft), Component.empty()));
+            }
+
+            update(player, timeLeft);
+            Feedback.lower(player);
+        });
     }
 
     @Override
     protected void onFinish() {
-        Bukkit.getServer().getOnlinePlayers().forEach(player -> update(player, 0));
+        Bukkit.getServer().getOnlinePlayers().forEach(player -> {
+            update(player, 0);
+            Feedback.pling(player);
+        });
         Bukkit.broadcast(Component.translatable("countdown.lobby.finish"));
     }
 
     private void update(@NotNull Player player, int timeLeft) {
         player.setLevel(timeLeft);
         player.setExp((float) timeLeft / super.duration());
-
     }
 }
