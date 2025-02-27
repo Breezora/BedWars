e3fffb3c4ef0c50efed17c767565fee507ac8efa
Merry <svgr1808@outlook.de>
2025-02-27 17:00:44 +0100

    [BED-127] Add lobby state
    Took 7 minutes

---
+++ src/main/java/net/alphalightning/bedwars/game/state/states/LobbyState.java
@@ -0,0 +1,19 @@
+package net.alphalightning.bedwars.game.state.states;
+
+import net.alphalightning.bedwars.game.state.AbstractGameState;
+import net.alphalightning.bedwars.game.state.GameStateContext;
+
+public class LobbyState extends AbstractGameState {
+
+    public LobbyState(GameStateContext context) {
+        super(context);
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void stop() {
+    }
+}
