fc701d64b0dd757e43381a83e57a0e0990136087
Max <mausetot467@gmail.com>
2025-03-05 16:56:28 +0100

    [BED-146] Teleport players to lobby spawn on join
    Took 24 minutes

--- src/main/java/net/alphalightning/bedwars/game/state/states/LobbyState.java
+++ src/main/java/net/alphalightning/bedwars/game/state/states/LobbyState.java
@@ -1,13 +1,30 @@
 package net.alphalightning.bedwars.game.state.states;
 
+import net.alphalightning.bedwars.BedWarsPlugin;
 import net.alphalightning.bedwars.game.state.AbstractGameState;
 import net.alphalightning.bedwars.game.state.GameStateContext;
+import net.alphalightning.bedwars.setup.map.jackson.LobbyLocations;
 import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
-public class LobbyState extends AbstractGameState {
+import java.io.IOException;
+import java.nio.file.Path;
 
-    public LobbyState(GameStateContext context) {
+import static net.alphalightning.bedwars.setup.map.LobbyConfiguration.LOBBY_FILE_NAME;
+
+public class LobbyState extends AbstractGameState implements Listener {
+
+    private final BedWarsPlugin plugin;
+
+    public LobbyState(GameStateContext context, BedWarsPlugin plugin) {
         super(context);
+        this.plugin = plugin;
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     @Override
@@ -19,4 +36,20 @@ public class LobbyState extends AbstractGameState {
     public void stop() {
         context.logger().info(Component.translatable("state.lobby.stop"));
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (plugin.gameStateContext().currentState() instanceof LobbyState) {
+            Player player = event.getPlayer();
+            try {
+                LobbyLocations lobbyLocations = plugin.jsonMapper().readValue(Path.of("maps/lobby.json").toFile(), LobbyLocations.class);
+                Location spawn = lobbyLocations.get("spawn").asBukkitLocation();
+                if (spawn == null) return;
+                player.teleport(spawn);
+            } catch (IOException exception) {
+                plugin.getLogger().severe("Could not read file " + LOBBY_FILE_NAME + ": " + exception.getMessage());
+            }
+        }
+    }
+
 }
