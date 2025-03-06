c25245a42277bb4449a74b048092910c8e46dd70
Max <mausetot467@gmail.com>
2025-03-06 10:02:03 +0100

    [BED-146] Debug again again
    Took 8 minutes

--- src/main/java/net/alphalightning/bedwars/game/state/states/LobbyState.java
+++ src/main/java/net/alphalightning/bedwars/game/state/states/LobbyState.java
@@ -1,6 +1,5 @@
 package net.alphalightning.bedwars.game.state.states;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import net.alphalightning.bedwars.BedWarsPlugin;
 import net.alphalightning.bedwars.game.state.AbstractGameState;
 import net.alphalightning.bedwars.game.state.GameStateContext;
@@ -14,8 +13,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 
 import static net.alphalightning.bedwars.setup.map.LobbyConfiguration.LOBBY_FILE_NAME;
 
@@ -46,10 +45,10 @@ public class LobbyState extends AbstractGameState implements Listener {
         if (context.currentState() instanceof LobbyState) {
             Player player = event.getPlayer();
             try {
-                ObjectMapper mapper = new ObjectMapper();
+                File lobbyFile = plugin.getDataFolder().toPath().resolve("maps").resolve("lobby.json").toFile();
 
-                LobbyLocations lobbyLocations = mapper.readValue(Paths.get("plugins/BedWars/maps/lobby.json").toFile(), LobbyLocations.class);
-                //LobbyLocations lobbyLocations = plugin.jsonMapper().
+                LobbyLocations lobbyLocations = plugin.jsonMapper().readValue(lobbyFile, LobbyLocations.class);
+                //LobbyLocations lobbyLocations =
                 Location spawn = lobbyLocations.get("spawn").asBukkitLocation();
 
                 if (spawn == null) return;
