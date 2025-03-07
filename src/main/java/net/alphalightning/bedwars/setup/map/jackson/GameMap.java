09c8785f0fe18be8bdb125440754789b234507c3
Merry <svgr1808@outlook.de>
2025-03-07 17:59:54 +0100

    [BED-167] Refactor to record class
    Took 2 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/jackson/GameMap.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/jackson/GameMap.java
@@ -1,26 +1,27 @@
 package net.alphalightning.bedwars.setup.map.jackson;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import net.alphalightning.bedwars.game.SpawnerType;
 
 import java.util.HashMap;
 import java.util.List;
 
-public class GameMap {
-
-    private final String name;
-    private final int teamSize;
-    private final int minBuildHeight;
-    private final int maxBuildHeight;
-    private final boolean slowIron;
-    private final JacksonLocation spectatorSpawn;
-    private final List<Team> teams;
-    private final List<SimpleJacksonLocation> shopVillager;
-    private final List<SimpleJacksonLocation> upgradeVillager;
-    private final HashMap<SpawnerType, List<SimpleJacksonLocation>> spawner;
+public record GameMap(String name, int teamSize, int minBuildHeight, int maxBuildHeight, boolean slowIron, JacksonLocation spectatorSpawn, List<Team> teams, List<SimpleJacksonLocation> shopVillager, List<SimpleJacksonLocation> upgradeVillager, HashMap<SpawnerType, List<SimpleJacksonLocation>> spawner) {
 
     @JsonCreator
-    public GameMap(String name, int teamSize, int minBuildHeight, int maxBuildHeight, boolean slowIron, JacksonLocation spectatorSpawn, List<Team> teams, List<SimpleJacksonLocation> shopVillager, List<SimpleJacksonLocation> upgradeVillager, HashMap<SpawnerType, List<SimpleJacksonLocation>> spawner) {
+    public GameMap(
+            @JsonProperty("name") String name,
+            @JsonProperty("teamSize") int teamSize,
+            @JsonProperty("minBuildHeight") int minBuildHeight,
+            @JsonProperty("maxBuildHeight") int maxBuildHeight,
+            @JsonProperty("slowIron") boolean slowIron,
+            @JsonProperty("spectatorSpawn") JacksonLocation spectatorSpawn,
+            @JsonProperty("teams") List<Team> teams,
+            @JsonProperty("shopVillager") List<SimpleJacksonLocation> shopVillager,
+            @JsonProperty("upgradeVillager") List<SimpleJacksonLocation> upgradeVillager,
+            @JsonProperty("spawner") HashMap<SpawnerType, List<SimpleJacksonLocation>> spawner) {
+
         this.name = name;
         this.teamSize = teamSize;
         this.minBuildHeight = minBuildHeight;
@@ -31,45 +32,5 @@ public class GameMap {
         this.shopVillager = shopVillager;
         this.upgradeVillager = upgradeVillager;
         this.spawner = spawner;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public int teamSize() {
-        return teamSize;
-    }
-
-    public int minBuildHeight() {
-        return minBuildHeight;
-    }
-
-    public int maxBuildHeight() {
-        return maxBuildHeight;
-    }
-
-    public boolean slowIron() {
-        return slowIron;
-    }
-
-    public JacksonLocation spectatorSpawn() {
-        return spectatorSpawn;
-    }
-
-    public List<Team> teams() {
-        return teams;
-    }
-
-    public List<SimpleJacksonLocation> shopVillager() {
-        return shopVillager;
-    }
-
-    public List<SimpleJacksonLocation> upgradeVillager() {
-        return upgradeVillager;
-    }
-
-    public HashMap<SpawnerType, List<SimpleJacksonLocation>> spawner() {
-        return spawner;
     }
 }
