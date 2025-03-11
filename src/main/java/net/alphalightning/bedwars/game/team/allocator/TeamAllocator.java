96a59e1a032dfef78dbf8642ab8c3686ebafad70
Merry <svgr1808@outlook.de>
2025-03-11 20:24:07 +0100

    [BED-172] Add javadocs
    Took 25 seconds

--- src/main/java/net/alphalightning/bedwars/game/team/allocator/TeamAllocator.java
+++ src/main/java/net/alphalightning/bedwars/game/team/allocator/TeamAllocator.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public interface TeamAllocator {
 
+    /**
+     * Teilt die Spieler in Teams anhand der optimalen Konfiguration auf.
+     *
+     * @param players    Liste der Spieler.
+     * @param maxTeams   Maximale Anzahl an Teams.
+     * @param maxTeamSize Maximale Spieleranzahl pro Team.
+     * @return Liste der Teams mit zugeordneten Spielern.
+     */
     @NotNull List<Team> allocateTeams(@NotNull List<Player> players, int maxTeams, int maxTeamSize);
 
 }
