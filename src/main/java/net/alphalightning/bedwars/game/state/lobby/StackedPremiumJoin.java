79f5e7791ff0cb830f31c8e4746301ed39de12b6
Merry <svgr1808@outlook.de>
2025-05-07 15:29:26 +0200

    [BED-148] Implement stacked premium join
    Took 13 minutes

--- src/main/java/net/alphalightning/bedwars/game/state/lobby/StackedPremiumJoin.java
+++ src/main/java/net/alphalightning/bedwars/game/state/lobby/StackedPremiumJoin.java
@@ -4,19 +4,30 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.Stack;
+
 public final class StackedPremiumJoin extends PremiumJoin {
+
+    private final Stack<Player> stack = new Stack<>();
 
     @Override
     public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission(PREMIUM_PERMISSION)) {
+            stack.push(player);
+        }
     }
 
     @Override
     public void onQuit(PlayerQuitEvent event) {
-
+        Player player = event.getPlayer();
+        if (!player.hasPermission(PREMIUM_PERMISSION)) {
+            stack.remove(player);
+        }
     }
 
     @Override
     public Player findKickablePlayer() {
-        return null;
+        return stack.isEmpty() ? null : stack.pop();
     }
 }
