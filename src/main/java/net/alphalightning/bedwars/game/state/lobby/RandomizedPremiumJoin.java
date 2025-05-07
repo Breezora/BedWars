e5cd34bdf012fd43d64e71c6b0bc76674298b695
Max <mausetot467@gmail.com>
2025-05-07 15:38:29 +0200

    [BED-148] Add Randomized PremiumJoin
    Took 15 minutes

--- src/main/java/net/alphalightning/bedwars/game/state/lobby/RandomizedPremiumJoin.java
+++ src/main/java/net/alphalightning/bedwars/game/state/lobby/RandomizedPremiumJoin.java
@@ -4,19 +4,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public final class RandomizedPremiumJoin extends PremiumJoin {
+
+    private final List<Player> list = new ArrayList<>();
+
 
     @Override
     public void onJoin(PlayerJoinEvent event) {
+        if (!event.getPlayer().hasPermission(PREMIUM_PERMISSION)) {
+            list.add(event.getPlayer());
+        }
     }
 
     @Override
     public void onQuit(PlayerQuitEvent event) {
-
+        list.remove(event.getPlayer());
     }
 
     @Override
     public Player findKickablePlayer() {
-        return null;
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
     }
+
 }
