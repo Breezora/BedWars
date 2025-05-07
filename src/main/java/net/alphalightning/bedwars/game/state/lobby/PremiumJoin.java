0022b0d561e2b096046db11a01228ee777162246
Merry <svgr1808@outlook.de>
2025-05-07 14:37:08 +0200

    [BED-148] Implement queued premium join
    Took 29 minutes

--- src/main/java/net/alphalightning/bedwars/game/state/lobby/PremiumJoin.java
+++ src/main/java/net/alphalightning/bedwars/game/state/lobby/PremiumJoin.java
@@ -1,14 +1,18 @@
 package net.alphalightning.bedwars.game.state.lobby;
 
 import net.alphalightning.bedwars.BedWarsPlugin;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 
 public abstract sealed class PremiumJoin
         implements Listener
         permits QueuedPremiumJoin, RandomizedPremiumJoin, StackedPremiumJoin {
+
+    protected static final String PREMIUM_PERMISSION = "bedwars.join.premium";
 
     protected final BedWarsPlugin plugin;
 
@@ -17,11 +21,27 @@ public abstract sealed class PremiumJoin
         Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
-    public abstract void onLogin(PlayerLoginEvent event);
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+
+        if (!isServerFull()) return;
+        if (!player.hasPermission(PREMIUM_PERMISSION)) return;
+
+        Player kickable = findKickablePlayer();
+        if (kickable == null) return;
+
+        kickable.kick(Component.translatable("lobby.kick.premium"));
+        event.allow();
+    }
 
     public abstract Player findKickablePlayer();
 
-    public boolean isServerFull() {
+    public boolean isPlayerKickable(Player player) {
+        return player != null && player.hasPermission(PREMIUM_PERMISSION);
+    }
+
+    private boolean isServerFull() {
         return Bukkit.getOnlinePlayers().size() >= Bukkit.getMaxPlayers();
     }
 
--- src/main/java/net/alphalightning/bedwars/game/state/lobby/QueuedPremiumJoin.java
+++ src/main/java/net/alphalightning/bedwars/game/state/lobby/QueuedPremiumJoin.java
@@ -2,20 +2,32 @@ package net.alphalightning.bedwars.game.state.lobby;
 
 import net.alphalightning.bedwars.BedWarsPlugin;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerLoginEvent;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final class QueuedPremiumJoin extends PremiumJoin {
+
+    private final Queue<Player> queue = new ConcurrentLinkedQueue<>();
 
     public QueuedPremiumJoin(BedWarsPlugin plugin) {
         super(plugin);
     }
 
     @Override
-    public void onLogin(PlayerLoginEvent event) {
-    }
-
-    @Override
     public Player findKickablePlayer() {
+        int queueSize = queue.size();
+
+        for (int i = 0; i < queueSize; i++) {
+            Player kickable = queue.poll();
+
+            if (isPlayerKickable(kickable)) {
+                return kickable;
+            }
+            if (kickable != null) {
+                queue.add(kickable);
+            }
+        }
         return null;
     }
 }
