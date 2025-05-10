795da6bbcb7e6fe3c53305637d69673c8ede8d26
Merry <svgr1808@outlook.de>
2025-05-10 14:37:31 +0200

    [BED-201] Add test command
    Took 38 seconds

--- src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
+++ src/main/java/net/alphalightning/bedwars/BedWarsPlugin.java
@@ -7,10 +7,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import de.eldoria.jacksonbukkit.JacksonPaper;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
-import net.alphalightning.bedwars.commands.CreateMapCommand;
-import net.alphalightning.bedwars.commands.ForceMapCommand;
-import net.alphalightning.bedwars.commands.StartCommand;
-import net.alphalightning.bedwars.commands.TestGuiCommand;
+import net.alphalightning.bedwars.commands.*;
 import net.alphalightning.bedwars.commands.cloud.sender.PaperCommandSource;
 import net.alphalightning.bedwars.commands.cloud.sender.PaperPlayerCommandSource;
 import net.alphalightning.bedwars.config.Configuration;
@@ -133,6 +130,8 @@ public class BedWarsPlugin extends JavaPlugin {
         }
         if (environment != Environment.PRODUCTION) {
             new TestGuiCommand(this).register(manager);
+            new TestWandCommand(this).register(manager);
+
             new CreateMapCommand(this, setupManager).register(manager);
 
             getComponentLogger().info(MiniMessage.miniMessage().deserialize("<green>Enabled <reset>map creation"));
---
+++ src/main/java/net/alphalightning/bedwars/commands/TestWandCommand.java
@@ -0,0 +1,31 @@
+package net.alphalightning.bedwars.commands;
+
+import net.alphalightning.bedwars.BedWarsPlugin;
+import net.alphalightning.bedwars.commands.cloud.sender.PaperCommand;
+import net.alphalightning.bedwars.commands.cloud.sender.PaperCommandSource;
+import net.alphalightning.bedwars.commands.cloud.sender.PaperPlayerCommandSource;
+import net.alphalightning.bedwars.util.SelectionWandTool;
+import org.bukkit.entity.Player;
+import org.incendo.cloud.CommandManager;
+import org.incendo.cloud.context.CommandContext;
+import org.jetbrains.annotations.NotNull;
+
+public class TestWandCommand extends PaperCommand<@NotNull BedWarsPlugin> {
+
+    public TestWandCommand(BedWarsPlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void register(@NotNull CommandManager<PaperCommandSource> commandManager) {
+        commandManager.command(commandManager.commandBuilder("testgui")
+                .senderType(PaperPlayerCommandSource.class)
+                .handler(this::runCommand)
+        );
+    }
+
+    private void runCommand(@NotNull CommandContext<PaperPlayerCommandSource> context) {
+        final Player player = (Player) context.sender().plattformSender();
+        new SelectionWandTool(player);
+    }
+}
