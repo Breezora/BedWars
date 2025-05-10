3e0cef14cf5ddf8e5e60c64d02430fb98dc4e751
Merry <svgr1808@outlook.de>
2025-05-10 14:36:53 +0200

    [BED-201] Implement wand tool
    Took 24 minutes

---
+++ src/main/java/net/alphalightning/bedwars/util/SelectionWandTool.java
@@ -0,0 +1,67 @@
+package net.alphalightning.bedwars.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.translation.GlobalTranslator;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import xyz.xenondevs.invui.item.ItemBuilder;
+
+public final class SelectionWandTool implements Listener {
+
+    private final Player owner;
+    private final ItemStack tool;
+    private Location first, second;
+
+    public SelectionWandTool(Player owner) {
+        this.owner = owner;
+        this.tool = new ItemBuilder(Material.ARROW)
+                .setCustomName(GlobalTranslator.render(Component.translatable("item.selection_wand"), owner.locale()))
+                .addLoreLines(
+                        GlobalTranslator.render(Component.translatable("item.selection_wand.left"), owner.locale()),
+                        GlobalTranslator.render(Component.translatable("item.selection_wand.right"), owner.locale()))
+                .hideTooltip(true)
+                .build();
+
+        owner.getInventory().clear();
+        owner.getInventory().setItem(0, tool);
+    }
+
+    public Location first() {
+        return first;
+    }
+
+    public Location second() {
+        return second;
+    }
+
+    @EventHandler
+    public void onToolUse(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getClickedBlock();
+
+        if (player != this.owner) return;
+        if (block == null) return;
+        if (!player.getInventory().getItemInMainHand().equals(tool)) return;
+
+        switch (event.getAction()) {
+            case LEFT_CLICK_BLOCK -> first(block.getLocation());
+            case RIGHT_CLICK_BLOCK -> second(block.getLocation());
+        }
+    }
+
+    private void first(Location first) {
+        this.first = first;
+        owner.sendMessage(Component.translatable("mapsetup.stage.16.first"));
+    }
+
+    private void second(Location second) {
+        this.second = second;
+        owner.sendMessage(Component.translatable("mapsetup.stage.16.second"));
+    }
+}
