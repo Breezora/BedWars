a12000870ee525c7499e626c102f393fe547d6ce
Merry <svgr1808@outlook.de>
2025-05-11 19:46:29 +0200

    [BED-204] Introduce the spawner protection stage
    Took 8 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/GameMapSetup.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/GameMapSetup.java
@@ -41,7 +41,8 @@ public final class GameMapSetup implements MapSetup {
     public static final int BED_CONFIGURATION_STAGE = 15;
     public static final int CUBOID_SELECTION_CONFIGURATION_STAGE = 16;
     public static final int FLOOD_FILL_CONFIGURATION_STAGE = 17;
-    public static final int COMPLETION_STAGE = 18;
+    public static final int SPAWNER_PROTECTION_CONFIGURATION_STAGE = 18;
+    public static final int COMPLETION_STAGE = 19;
 
     private final CancelStage cancelStage;
 
@@ -102,6 +103,7 @@ public final class GameMapSetup implements MapSetup {
             case BED_CONFIGURATION_STAGE -> new BedConfigurationStage(plugin, player, this).run();
             case CUBOID_SELECTION_CONFIGURATION_STAGE -> new CuboidConfigurationStage(plugin, player, this).run();
             case FLOOD_FILL_CONFIGURATION_STAGE -> new FloodFillConfigurationStage(plugin, player, this).run();
+            case SPAWNER_PROTECTION_CONFIGURATION_STAGE -> new SpawnerProtectionConfigurationStage(plugin, player, this).run();
             case COMPLETION_STAGE -> new CompleteSetupStage(plugin, player, this, fileName, binaryFileName, false).run();
             default -> cancelStage.run();
         }
