47b12636a94fc2f2d6b59fde1fef37010b83044e
Max <mausetot467@gmail.com>
2025-05-11 20:34:48 +0200

    [BED-98] Add some more logic
    Took 12 minutes

--- src/main/java/net/alphalightning/bedwars/game/ui/shop/item/items/BuyableItem.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/shop/item/items/BuyableItem.java
@@ -14,6 +14,7 @@ import xyz.xenondevs.invui.item.ItemProvider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class BuyableItem extends AbstractItem {
@@ -49,11 +50,29 @@ public class BuyableItem extends AbstractItem {
         }
         return builder.setLore(lore);
     }
-    private String getPrice(@NotNull Player viewer) {
+    private String getPriceTag(@NotNull Player viewer) {
         return Objects.requireNonNull(plugin.translator().getMiniMessageString(itemLore.getFirst(), viewer.locale()));
+    }
+
+    private String getCurrency(String toCheck) {
+        Map<String, String> COLOR_TO_CURRENCY = Map.of(
+                "white", "IRON",
+                "gold", "GOLD",
+                "dark_green", "EMERALD",
+                "aqua", "DIAMOND"
+        );
+
+        String color = toCheck.split("<")[2].split(">")[0];
+        return COLOR_TO_CURRENCY.get(color);
+    }
+
+    private int extractAmount(String input) {
+        String[] parts = input.split(">");
+        String amountPart = parts[2].trim().split(" ")[0];
+        return Integer.parseInt(amountPart);
     }
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
-        System.out.println(getPrice(player));
+        System.out.println("Dieses Item kostet: " + getCurrency(getPriceTag(player)) + " " + extractAmount(getPriceTag(player)));
     }
 }
