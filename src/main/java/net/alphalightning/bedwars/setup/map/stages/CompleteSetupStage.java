9da3fe2256e86348c8d8e4edd13dcd5972bbc8e9
Merry <svgr1808@outlook.de>
2025-05-11 18:08:33 +0200

    [BED-202] Add filename to message
    Took 3 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/stages/CompleteSetupStage.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/stages/CompleteSetupStage.java
@@ -9,17 +9,19 @@ import org.bukkit.entity.Player;
 public class CompleteSetupStage extends Stage {
 
     private final String fileName;
+    private final String binaryFileName;
     private final boolean isLobbySetup;
 
-    public CompleteSetupStage(BedWarsPlugin plugin, Player player, MapSetup setup, String fileName, boolean isLobbySetup) {
+    public CompleteSetupStage(BedWarsPlugin plugin, Player player, MapSetup setup, String fileName, String binaryFileName, boolean isLobbySetup) {
         super(plugin, player, setup);
         this.fileName = fileName;
+        this.binaryFileName = binaryFileName;
         this.isLobbySetup = isLobbySetup;
     }
 
     @Override
     public void run() {
-        player.sendMessage(Component.translatable(isLobbySetup ? "lobbysetup.finish" : "mapsetup.finish", Component.text(fileName)));
+        player.sendMessage(Component.translatable(isLobbySetup ? "lobbysetup.finish" : "mapsetup.finish", Component.text(fileName), Component.text(binaryFileName)));
         Feedback.complete(player);
     }
 }
