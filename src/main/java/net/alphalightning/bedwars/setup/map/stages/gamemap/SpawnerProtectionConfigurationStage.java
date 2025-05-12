0f7fa458f01d52422544ceee50a9542c94f427a7
Merry <svgr1808@outlook.de>
2025-05-12 19:09:59 +0200

    [BED-190] Use another setup elsewhere
    Took 6 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/stages/gamemap/SpawnerProtectionConfigurationStage.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/stages/gamemap/SpawnerProtectionConfigurationStage.java
@@ -87,7 +87,7 @@ public class SpawnerProtectionConfigurationStage extends Stage implements Approv
         RegionInformation information = RegionUtil.createRegionInformation(selection);
 
         informationList.add(information);
-        visualizationManager.registerTask(gameMapSetup, new BoundingBoxRenderer<List<Block>>(plugin, gameMapSetup).render(selection.corners(), Color.fromRGB(0xF06562).asRGB()));
+        visualizationManager.registerTask(setup, new BoundingBoxRenderer<List<Block>>(plugin, gameMapSetup).render(selection.corners(), Color.fromRGB(0xF06562).asRGB()));
     }
 
     @EventHandler
