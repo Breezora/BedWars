b7a456a06b3972d65679ed0e88cd6f153241d3e4
Merry <svgr1808@outlook.de>
2025-05-12 15:49:04 +0200

    [BED-190] Refactor inventory recovery
    Took 2 minutes

--- src/main/java/net/alphalightning/bedwars/setup/map/stages/CancelStage.java
+++ src/main/java/net/alphalightning/bedwars/setup/map/stages/CancelStage.java
@@ -43,8 +43,6 @@ public class CancelStage extends Stage {
             return;
         }
 
-        player.getInventory().clear();
-
         event.setCancelled(true);
         setupManager.cancelSetup(player);
     }
