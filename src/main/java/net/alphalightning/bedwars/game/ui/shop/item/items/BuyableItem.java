9bcd32e336a94d080e87d6c1681e46c71f863cfe
Max <mausetot467@gmail.com>
2025-07-11 08:52:58 +0200

    [BED-98] Give Player correct Wool color on buy
    Took 36 minutes

--- src/main/java/net/alphalightning/bedwars/game/ui/shop/item/items/BuyableItem.java
+++ src/main/java/net/alphalightning/bedwars/game/ui/shop/item/items/BuyableItem.java
@@ -1,6 +1,8 @@
 package net.alphalightning.bedwars.game.ui.shop.item.items;
 
 import net.alphalightning.bedwars.BedWarsPlugin;
+import net.alphalightning.bedwars.game.team.Team;
+import net.alphalightning.bedwars.util.PlayerUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.translation.GlobalTranslator;
 import org.bukkit.Material;
@@ -28,6 +30,7 @@ public class BuyableItem extends AbstractItem {
     private final String itemNameKey;
     private final List<String> itemLore;
     private final int itemAmount;
+    private List<Team> teams;
 
     public BuyableItem(BedWarsPlugin plugin, Material itemMaterial, String itemNameKey, int itemAmount, String... itemLore) {
         this.plugin = plugin;
@@ -76,24 +79,41 @@ public class BuyableItem extends AbstractItem {
     }
     @Override
     public void handleClick(@NotNull ClickType clickType, @NotNull Player player, @NotNull Click click) {
-        ItemStack currency = getCurrency(getPriceTag(player));
-        int cost = extractAmount(getPriceTag(player));
+        ItemStack boughtItem = getItemProvider(player).get().clone();
+        for (Team team : teams) {
+            for (Player clickedPlayer : team.players()) {
+                if(clickedPlayer != player) return;
 
-        if (!hasEnoughCurrency(player, currency, cost)) {
-            switch (currency.getType()) {
-                case IRON_INGOT -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-iron"));
-                case GOLD_INGOT -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-gold"));
-                case EMERALD -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-emerald"));
-                case DIAMOND -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-diamond"));
-            }
-            player.playSound(player.getLocation(), Sound.ENTITY_ALLAY_HURT, 0.5F, 1.0F); //TODO: Change sound to hypixel sound
-        } else {
-            if(hasNotEnoughSpace(player)) {
-                player.sendMessage(Component.translatable("player.inventory.full"));
-                player.playSound(player.getLocation(), Sound.ENTITY_ALLAY_HURT, 0.5F, 1.0F); //TODO: Change sound to hypixel sound
-            } else {
-                removeCurrency(player, currency, extractAmount(getPriceTag(player)));
-                player.getInventory().addItem(getItemProvider(player).get());
+                ItemStack currency = getCurrency(getPriceTag(player));
+                int cost = extractAmount(getPriceTag(player));
+
+                if (!hasEnoughCurrency(player, currency, cost)) {
+                    switch (currency.getType()) {
+                        case IRON_INGOT -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-iron"));
+                        case GOLD_INGOT -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-gold"));
+                        case EMERALD -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-emerald"));
+                        case DIAMOND -> player.sendMessage(Component.translatable("gui.shop.itemshop.buyable.lore.not-enough-diamond"));
+                    }
+                    player.playSound(player.getLocation(), Sound.ENTITY_ALLAY_HURT, 0.5F, 1.0F); //TODO: Change sound to hypixel sound
+                } else {
+                    if(hasNotEnoughSpace(player)) {
+                        player.sendMessage(Component.translatable("player.inventory.full"));
+                        player.playSound(player.getLocation(), Sound.ENTITY_ALLAY_HURT, 0.5F, 1.0F); //TODO: Change sound to hypixel sound
+                    } else {
+                        removeCurrency(player, currency, extractAmount(getPriceTag(player)));
+                        Material type = boughtItem.getType();
+                        if(type.name().endsWith("_WOOL")) {
+                            ItemBuilder builder = new ItemBuilder(Objects.requireNonNull(
+                                    Material.getMaterial(PlayerUtil.materialString(team.color()) + "_WOOL")))
+                                    .setAmount(16);
+                            ItemStack boughtWool = builder.build();
+                            player.getInventory().addItem(boughtWool);
+
+                        }
+                    }
+                }
+
+
             }
         }
 
